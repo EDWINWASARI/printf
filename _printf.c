20f84bef8dcedbdcd09636312016b3ede01fbf94
EDWINWASARI <edwinwasari@gmail.com>
2023-10-17 11:13:06 -0700

    a function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,61 +1,69 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * print_format - That format handles format specifiers
- * @format: The format specifier to be handled
- * @args: The va_list containing list of arguments.
- * Return: Returns the number of characters printed
- */
-
-int print_format(char format, va_list args)
-{
-int print = 0;
-switch (format)
-{
-case 'c':
-print += putchar(va_arg(args, int));
-break;
-case 's':
-print += write(1, va_arg(args, char*), strlen(va_arg(args, char*)));
-break;
-case '%':
-print += write(1, "%", 1);
-break;
-default:
-print += write(1, &format, 1);
-}
-return (print);
-}
-
-/**
- * _printf - contains specifiers to be handled
+ * _printf - Produces output according to a format.
  * @format: A format string with format specifiers.
- * Handled specifiers: %c, %s, and %%
- * Return: Returns the number of characters printed.
+ * Return: The number of characters printed.
  */
 
 int _printf(const char *format, ...)
 {
-int addition = 0;
+int class = 0;
 va_list edu;
+if (format == NULL)
+return (-4);
 va_start(edu, format);
+class = print_format(format, edu);
+va_end(edu);
+return (class);
+}
 
-while (*format != '\0')
+/**
+ * print_format - Process the format string and write characters.
+ * @format: A format string with format specifiers.
+ * @edu: The va_list containing the arguments.
+ * Return: The number of characters printed.
+ */
+
+int print_format(const char *format, va_list edu)
 {
-if (*format == '%')
+int class = 0;
+while (*format)
 {
-addition += print_format(*(++format), edu);
+if (*format != '%')
+{
+write(1, format, 1);
+class++;
 }
 else
 {
-addition += write(1, format, 1);
+format++;
+if (*format == '\0')
+break;
+if (*format == '%')
+{
+write(1, format, 1);
+class++;
 }
-++format;
+else if (*format == 'c')
+{
+char c;
+c = (char) va_arg(edu, int);
+write(1, &c, 1);
+class++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(edu, char*);
+int str_len = 0;
+while (str[str_len] != '\0')
+write(1, str, str_len);
+class += str_len;
+}
+format++;
+}
 }
 va_end(edu);
-return (addition);
+return (class);
 }
+
--- main.h
+++ main.h
@@ -3,8 +3,10 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int _printf(const char *format, ...);
-int print_format(char format, va_list args);
+int print_format(const char *format, va_list edu);
+
 
 #endif
