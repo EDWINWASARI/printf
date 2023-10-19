9b29186e117f7a8189f074516d20bb12432dee68
EDWINWASARI <edwinwasari@gmail.com>
2023-10-19 09:11:26 -0700

    printf.c

--- main.h
+++ main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-#include <string.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <limits.h>
+
 
 int print_format(char format, va_list args);
 int _printf(const char *format, ...);
