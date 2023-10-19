4c7d2b3deed5054cbdbfe25455f369b3e4b3972a
EDWINWASARI <edwinwasari@gmail.com>
2023-10-19 09:05:25 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,50 +1,17 @@
 #include "main.h"
 
 /**
- * print_format - Prints characters based on the format specifier.
- * @format: The format specifier ('c', 's', or '%').
- * @args: The va_list containing the arguments.
- *
- * Return: The number of characters printed, addition
- */
-int print_format(char format, va_list args)
-{
-int addition = 0;
-switch (format)
-{
-case 'c':
-{
-char c = va_arg(args, int);
-addition += write(1, &c, 1);
-break;
-}
-case 's':
-{
-char *str = va_arg(args, char*);
-int len = strlen(str);
-addition += write(1, str, len);
-break;
-}
-case '%':
-addition += write(1, "%", 1);
-break;
-default:
-addition += write(1, &format, 1);
-}
-return (addition);
-}
-
-/**
  * _printf - Produces output according to a format.
  * @format: A format string with format specifiers.
- * Return: The number of characters printed, addition.
+ *
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 va_list args;
 int addition = 0;
+if (format == NULL)
 {
-if (!format || (format[0] == '%' && format[1] == '\0'))
 return (-1);
 }
 va_start(args, format);
@@ -57,7 +24,8 @@ if (*(format + 1) == '\0')
 va_end(args);
 return (-1);
 }
-addition += print_format(*(++format), args);
+format++;
+addition += print_format(*format, args);
 }
 else
 {
@@ -66,5 +34,42 @@ addition += write(1, format, 1);
 format++;
 }
 va_end(args);
+return (addition);
+}
+
+
+/**
+ * print_format - Handles printing for format specifiers.
+ * @format: The format specifier ('c', 's', or '%').
+ * @args: The va_list containing the arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_format(char format, va_list args)
+{
+int addition = 0;
+int len = 0;
+switch (format)
+{
+case 'c':
+{
+char c = va_arg(args, int);
+addition += write(1, &c, 1);
+break;
+}
+case 's':
+{
+char *str = va_arg(args, char*);
+while (str[len] != '\0')
+len++;
+addition += write(1, str, len);
+break;
+}
+case '%':
+addition += write(1, "%", 1);
+break;
+default:
+addition += write(1, &format, 1);
+}
 return (addition);
 }
