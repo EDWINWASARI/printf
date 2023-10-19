d8425a784b8db83ebdc75f030c4620460315c0e5
EDWINWASARI <edwinwasari@gmail.com>
2023-10-19 04:01:50 -0700

    printf

--- printf.c
+++ printf.c
@@ -44,7 +44,7 @@ int _printf(const char *format, ...)
 va_list args;
 int addition = 0;
 {
-if (format == NULL)
+if (!format || (format[0] == '%' && format[1] == '\0'))
 return (-1);
 }
 va_start(args, format);
